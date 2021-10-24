327f2e7b9f369bffbf157cb0eec6d48434f6f4fd
SungJin Kim <sungjinkimm@gmail.com>
2021-10-24 13:37:04 -0400

    dd

--- server.js
+++ server.js
@@ -9,29 +9,36 @@ const cors = require('cors');
 dotenv.config({ path: './config/config.env' });
 
 connectDB();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
 
 const posts = require('./routes/posts');
-
-const app = express();
-
-app.use(cors());
-
-app.use(express.json());
 
 // if (process.env.NODE_ENV === 'development') {
 //   app.use(morgan('dev'));
 // }
 
 app.use('/api/v1/posts', posts);
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
 }
 
-const PORT = process.env.PORT || 5000;
+// if (process.env.NODE_ENV === 'production') {
+//   console.log('production');
+//   app.use(express.static('client/build'));
+
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//   );
+// }
+
 app.listen(PORT, () => {
   console.log(
     `Server live as ${process.env.NODE_ENV} on ${PORT}`.yellow.underline.bold

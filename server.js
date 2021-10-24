55fa3d80bf4ee29ff821a4d5673c7d1c9278c242
SungJin Kim <sungjinkimm@gmail.com>
2021-10-24 11:59:49 -0400

    react build complete

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
@@ -22,6 +23,13 @@ app.use(express.json());
 // }
 
 app.use('/api/v1/posts', posts);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
